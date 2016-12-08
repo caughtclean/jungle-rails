175a4997334f4dfa55490164094ab1f859def0c3
Nathan Eyerley <eyerleyn@gmail.com>
2016-12-08 03:54:15 +0000

    added add category function to admin

--- config/routes.rb
+++ config/routes.rb
@@ -12,8 +12,12 @@ Rails.application.routes.draw do
 
   resources :orders, only: [:create, :show]
 
+
+
   namespace :admin do
     root to: 'dashboard#show'
+
+    resources :categories, only: [:index, :new, :create]
     resources :products, except: [:edit, :update, :show]
   end
 
