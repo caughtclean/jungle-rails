82ae657909ccd02ad0b283d25a467652823ea11e
Nathan Eyerley <eyerleyn@gmail.com>
2016-12-10 04:40:22 +0000

    finished most features on user reviews

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,27 @@
+class ReviewsController < ApplicationController
+  before_action :require_login
+
+  def new
+    @review = Review.new
+  end
+
+  def create
+    product = Product.find(params[:product_id])
+    @review = product.reviews.create(review_params)
+    @review.user = current_user
+    @review.save
+    redirect_to "/products/#{product.id}", flash: {review_success: "Review posted!"}
+  end
+
+  def destroy
+    @review = Review.find(params[:id])
+    @review.destroy
+    redirect_to "/products/#{params[:product_id]}", flash: {review_success: "That review was dumb anyway!"}
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:product_id, :description, :rating)
+  end
+end
